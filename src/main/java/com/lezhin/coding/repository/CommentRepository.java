75c4c981445a0f28f8b28c6c99388a7aac7aa931
noveltaker <pnw123456@icloud.com>
2022-05-29 14:22:59 +0900

    chore: findTopByType logic remove

--- src/main/java/com/lezhin/coding/repository/CommentRepository.java
+++ src/main/java/com/lezhin/coding/repository/CommentRepository.java
@@ -1,13 +1,9 @@
 package com.lezhin.coding.repository;
 
-import com.lezhin.coding.constants.EvaluationType;
 import com.lezhin.coding.domain.Comment;
 import com.lezhin.coding.domain.support.CommentKey;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, CommentKey> {
