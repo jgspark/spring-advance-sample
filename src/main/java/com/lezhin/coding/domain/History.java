f930a2621a86621bfa3ba0f52b1cd1f3cd832b57
noveltaker <pnw123456@icloud.com>
2022-05-30 19:45:58 +0900

    feat: select 히스토리 추가

---
+++ src/main/java/com/lezhin/coding/domain/History.java
@@ -0,0 +1,26 @@
+package com.lezhin.coding.domain;
+
+import com.lezhin.coding.domain.support.AbstractBaseDate;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class History extends AbstractBaseDate {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @OneToOne
+  @JoinColumn(name = "contents_id")
+  private Contents contents;
+}
---
+++ src/main/java/com/lezhin/coding/repository/HistoryRepository.java
@@ -0,0 +1,8 @@
+package com.lezhin.coding.repository;
+
+import com.lezhin.coding.domain.History;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HistoryRepository extends JpaRepository<History, Long> {}
