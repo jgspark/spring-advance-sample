59648380bf7be515395b84a2829eee27ae0ac661
noveltaker <pnw123456@icloud.com>
2022-05-31 17:59:11 +0900

    test: 최근 1주일 등록된 사용자 중 성인 조회 api 테스트 케이스

--- src/test/java/com/lezhin/coding/repository/HistoryRepositoryTest.java
+++ src/test/java/com/lezhin/coding/repository/HistoryRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.lezhin.coding.repository;
 
 import com.lezhin.coding.config.JPAConfiguration;
+import com.lezhin.coding.constants.AdultType;
 import com.lezhin.coding.domain.Contents;
 import com.lezhin.coding.domain.History;
 import com.lezhin.coding.domain.User;
@@ -8,6 +9,8 @@ import com.lezhin.coding.mock.ContentsMock;
 import com.lezhin.coding.mock.HistoryMock;
 import com.lezhin.coding.mock.UserMock;
 import com.lezhin.coding.service.dto.HistoryInfo;
+import com.lezhin.coding.service.dto.HistoryUser;
+import com.lezhin.coding.utils.DateUtil;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +20,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Date;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
@@ -36,6 +38,8 @@ class HistoryRepositoryTest {
 
   private Contents contents;
 
+  private History entity;
+
   @BeforeEach
   void init() {
     user = userRepository.saveAndFlush(UserMock.createdMock());
@@ -48,7 +52,7 @@ class HistoryRepositoryTest {
 
     History mock = HistoryMock.createdMock(user, contents);
 
-    History entity = historyRepository.save(mock);
+    entity = historyRepository.save(mock);
 
     Assertions.assertEquals(entity.getUser().getId(), mock.getUser().getId());
     Assertions.assertEquals(entity.getContents().getId(), entity.getContents().getId());
@@ -58,11 +62,20 @@ class HistoryRepositoryTest {
   @Nested
   class Select {
 
-    private History history;
+    private List<History> mocks;
 
     @BeforeEach
     void init() {
-      history = historyRepository.save(HistoryMock.createdMock(user, contents));
+
+      mocks =
+          historyRepository.saveAll(
+              List.of(
+                  HistoryMock.createdMock(user, contents),
+                  HistoryMock.createdMock(user, contents),
+                  HistoryMock.createdMock(user, contents),
+                  HistoryMock.createdMock(user, contents),
+                  HistoryMock.createdMock(user, contents)));
+
       historyRepository.flush();
     }
 
@@ -70,28 +83,81 @@ class HistoryRepositoryTest {
     @DisplayName("조회 이력 API 테스트 케이스")
     void findAllProjectedBy() {
 
-      Page<HistoryInfo> mocks = HistoryMock.createdPageList(user, contents);
-
       PageRequest pageable = PageRequest.of(0, 10);
 
       Page<HistoryInfo> entities =
           historyRepository.findAllProjectedBy(pageable, HistoryInfo.class);
 
-      List<HistoryInfo> mockContent = mocks.getContent();
+      Assertions.assertFalse(entities.isEmpty());
 
       List<HistoryInfo> entitiesContent = entities.getContent();
 
-      Assertions.assertEquals(entitiesContent.size(), mockContent.size());
+      entitiesContent.forEach(
+          entity -> {
+            History mock =
+                mocks.stream()
+                    .filter(m -> m.getId().equals(entity.getId()))
+                    .findFirst()
+                    .orElseThrow();
 
-      HistoryInfo entity = entitiesContent.get(0);
+            Assertions.assertEquals(entity.getUserName(), mock.getUser().getUserName());
+            Assertions.assertEquals(entity.getContentsName(), mock.getContents().getName());
+            Assertions.assertEquals(entity.getContentsType(), mock.getContents().getType());
+          });
+    }
+  }
 
-      HistoryInfo mock = mockContent.get(0);
+  @Nested
+  class SelectHistoryUser {
 
-      Assertions.assertEquals(entity.getId(), mock.getId());
-      Assertions.assertEquals(entity.getUserName(), mock.getUserName());
-      Assertions.assertEquals(entity.getContentsName(), mock.getContentsName());
-      Assertions.assertEquals(entity.getContentsType(), mock.getContentsType());
-      Assertions.assertNotNull(entity.getCreatedDate());
+    private List<History> mocks;
+
+    @BeforeEach
+    void init() {
+
+      mocks =
+          historyRepository.saveAll(
+              List.of(
+                  HistoryMock.createdMock(user, contents),
+                  HistoryMock.createdMock(user, contents),
+                  HistoryMock.createdMock(user, contents),
+                  HistoryMock.createdMock(user, contents),
+                  HistoryMock.createdMock(user, contents)));
+
+      historyRepository.flush();
+    }
+
+    @Test
+    @DisplayName("최근 1주일 등록된 사용자 중 성인 조회 api 테스트 케이스")
+    void findByCreatedDateBetweenAndContents_AdultType() {
+
+      Date startDate = DateUtil.minus(new Date(), -7);
+
+      Date endDate = DateUtil.plus(new Date(), 1);
+
+      PageRequest pageable = PageRequest.of(0, 10);
+
+      Page<HistoryUser> entities =
+          historyRepository.findByCreatedDateBetweenAndContents_AdultType(
+              pageable, startDate, endDate, AdultType.ADULT, 3L);
+
+      Assertions.assertFalse(entities.isEmpty());
+
+      List<HistoryUser> entitiesContent = entities.getContent();
+
+      entitiesContent.forEach(
+          entity -> {
+            History mock =
+                mocks.stream()
+                    .filter(m -> m.getId().equals(entity.getId()))
+                    .findFirst()
+                    .orElseThrow();
+
+            Assertions.assertEquals(entity.getUserName(), mock.getUser().getUserName());
+            Assertions.assertEquals(entity.getUserEmail(), mock.getUser().getUserEmail());
+            Assertions.assertEquals(entity.getGender(), mock.getUser().getGender());
+            Assertions.assertEquals(entity.getType(), mock.getUser().getType());
+          });
     }
   }
 }
