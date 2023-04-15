e2a36c30b584b6fe056d176600e869d40f9815f0
spark <pnw123456@icloud.com>
2023-04-15 12:44:42 +0900

    test: #2 comment domain 테스트 케이스 추가

---
+++ src/test/java/com/webtoon/coding/domain/comment/CommentTest.java
@@ -0,0 +1,103 @@
+package com.webtoon.coding.domain.comment;
+
+import com.webtoon.coding.core.exception.DomainException;
+import com.webtoon.coding.core.exception.MsgType;
+import com.webtoon.coding.domain.common.Verifier;
+import com.webtoon.coding.domain.contents.Contents;
+import com.webtoon.coding.domain.user.User;
+import com.webtoon.coding.mock.args.comment.CommentVerifyFailCommentArgs;
+import com.webtoon.coding.mock.args.comment.CommentVerifyFailTypeArgs;
+import com.webtoon.coding.mock.args.comment.CommentVerifySuccessArgs;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("코멘트 도메인에서")
+class CommentTest {
+
+    @Mock
+    private Verifier<Comment> verifier;
+
+    @Nested
+    @DisplayName("작성 메소드는")
+    public class Write {
+
+        @DisplayName("성공를 한다.")
+        @ParameterizedTest(name = "댓글은 {0} 이다.")
+        @ArgumentsSource(CommentVerifySuccessArgs.class)
+        public void testWriteSuccess(String stringArgs) {
+
+            Comment comment = Comment.of(
+                    stringArgs,
+                    Evaluation.GOOD,
+                    User.builder().build(),
+                    Contents.builder().build()
+            );
+
+            doNothing().when(verifier).verify(any());
+
+            assertDoesNotThrow(() -> comment.write(verifier));
+
+            verify(verifier, times(1)).verify(any());
+        }
+
+        @DisplayName("실패를 한다.")
+        @ParameterizedTest(name = "댓글은 {0} 이다.")
+        @ArgumentsSource(CommentVerifyFailCommentArgs.class)
+        public void testWriteFailByCommentArgs(String stringArgs) {
+
+            Comment comment = Comment.of(
+                    stringArgs,
+                    Evaluation.GOOD,
+                    User.builder().build(),
+                    Contents.builder().build()
+            );
+
+            doThrow(new DomainException(MsgType.CommentDataException))
+                    .when(verifier)
+                    .verify(any());
+
+            DomainException e = assertThrows(DomainException.class, () -> comment.write(verifier));
+
+            verify(verifier, times(1)).verify(any());
+
+            assertEquals(e.getMsgType(), MsgType.CommentDataException);
+            assertEquals(e.getMessage(), MsgType.CommentDataException.getMessage());
+        }
+
+
+        @DisplayName("실패를 한다.")
+        @ParameterizedTest(name = "평가는 {0} 이다.")
+        @ArgumentsSource(CommentVerifyFailTypeArgs.class)
+        public void testWriteFailByTypeArgs(Evaluation type) {
+
+            Comment comment = Comment.of(
+                    "test",
+                    type,
+                    User.builder().build(),
+                    Contents.builder().build()
+            );
+
+            doThrow(new DomainException(MsgType.EvaluationDataException))
+                    .when(verifier)
+                    .verify(any());
+
+            DomainException e = assertThrows(DomainException.class, () -> comment.write(verifier));
+
+            verify(verifier, times(1)).verify(any());
+
+            assertEquals(e.getMsgType(), MsgType.EvaluationDataException);
+            assertEquals(e.getMessage(), MsgType.EvaluationDataException.getMessage());
+        }
+    }
+
+}
