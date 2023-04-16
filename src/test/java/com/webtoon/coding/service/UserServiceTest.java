b949da67ba33d7ee005d202b001bbf7bf2e0f472
spark <pnw123456@icloud.com>
2023-04-16 21:44:18 +0900

    test: #4 UserService 삭제 테스트 케이스 추가

--- src/test/java/com/webtoon/coding/service/UserServiceTest.java
+++ src/test/java/com/webtoon/coding/service/UserServiceTest.java
@@ -1,53 +1,43 @@
 package com.webtoon.coding.service;
 
-import com.webtoon.coding.infra.repository.comment.CommentRepository;
-import com.webtoon.coding.infra.repository.history.HistoryRepository;
-import com.webtoon.coding.infra.repository.user.UserRepository;
+import com.webtoon.coding.domain.user.UserRemover;
 import com.webtoon.coding.service.user.UserService;
 import com.webtoon.coding.service.user.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 class UserServiceTest {
 
-  private UserService userService;
+    private UserService userService;
 
-  @Mock private UserRepository userRepository;
+    @Mock
+    private UserRemover userRemover;
 
-  @Mock private HistoryRepository historyRepository;
+    @BeforeEach
+    void init() {
+        this.userService = new UserServiceImpl(userRemover);
+    }
 
-  @Mock private CommentRepository commentRepository;
+    @Test
+    @DisplayName("삭제 로직 테스트 케이스")
+    void removeUser() {
 
-  @BeforeEach
-  void init() {
-    this.userService = new UserServiceImpl(commentRepository, historyRepository, userRepository);
-  }
+        doNothing().when(userRemover).remove(any());
 
-  @Test
-  @DisplayName("삭제 로직 테스트 케이스")
-  void removeUser() {
+        userService.removeUser(1L);
 
-    BDDMockito.willDoNothing().given(commentRepository).deleteById_UserId(any());
-    BDDMockito.willDoNothing().given(historyRepository).deleteByUser_Id(any());
-    BDDMockito.willDoNothing().given(userRepository).deleteById(any());
+        verify(userRemover, times(1)).remove(any());
 
-    userService.removeUser(1L);
-
-    BDDMockito.then(commentRepository).should().deleteById_UserId(any());
-
-    BDDMockito.then(historyRepository).should().deleteByUser_Id(any());
-
-    BDDMockito.then(userRepository).should().deleteById(any());
-  }
+    }
 }
