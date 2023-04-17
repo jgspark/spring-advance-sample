2c906d37aeb7561cfe16fee72b33f481f319035d
jgspark <pnw123456@icloud.com>
2023-04-17 19:27:41 +0900

    test: #3 Content Controller 테스트 케이스 추가

--- src/test/java/com/webtoon/coding/web/contents/ContentsControllerTest.java
+++ src/test/java/com/webtoon/coding/web/contents/ContentsControllerTest.java
@@ -14,9 +14,9 @@ import com.webtoon.coding.mock.JsonUtil;
 import com.webtoon.coding.service.contents.ContentsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
@@ -28,10 +28,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -47,139 +49,219 @@ class ContentsControllerTest {
 
     @BeforeEach
     void init() {
-        this.mockMvc =
-                MockMvcBuilders.standaloneSetup(new ContentsController(contentsService))
-                        .setControllerAdvice(new RestExceptionHandler())
-                        .addFilter(new CharacterEncodingFilter("UTF-8", true))
-                        .build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new ContentsController(contentsService)).setControllerAdvice(new RestExceptionHandler()).addFilter(new CharacterEncodingFilter("UTF-8", true)).build();
     }
 
-    @Test
+    @Nested
     @DisplayName("좋아요가 가장 많은 작품 3개 API")
-    void getTopContents() throws Exception {
+    public class GetTopContents {
 
-        List<TopContents> mocks = ContentsMock.createdTopContentsList();
+        @Test
+        @DisplayName("는 성공적으로 동작")
+        void testGetTopContents() throws Exception {
 
-        BDDMockito.given(contentsService.getTopContents(any())).willReturn(mocks);
+            List<TopContents> mocks = ContentsMock.createdTopContentsList();
 
-        ResultActions action =
-                mockMvc
-                        .perform(
-                                MockMvcRequestBuilders.get("/top-contents")
-                                        .param("type", Evaluation.GOOD.name())
-                                        .contentType(MediaType.APPLICATION_JSON)
-                                        .characterEncoding("UTF-8"))
-                        .andDo(print());
+            when(contentsService.getTopContents(any())).thenReturn(mocks);
 
-        BDDMockito.then(contentsService).should().getTopContents(any());
+            ResultActions action = mockMvc.perform(MockMvcRequestBuilders.get("/top-contents").param("type", Evaluation.GOOD.name())
+                    .contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
+                    .andDo(print());
 
-        action
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0]['id']").value(mocks.get(0).getId()))
-                .andExpect(jsonPath("$[0]['name']").value(mocks.get(0).getName()))
-                .andExpect(jsonPath("$[0]['author']").value(mocks.get(0).getAuthor()))
-                .andExpect(jsonPath("$[0]['type']").value(mocks.get(0).getType().name()))
-                .andExpect(jsonPath("$[0]['coin']").value(mocks.get(0).getCoin()))
-                .andExpect(jsonPath("$[0]['openDate']").value(mocks.get(0).getOpenDate()))
-                .andExpect(jsonPath("$[0]['sum']").value(mocks.get(0).getSum()));
+            verify(contentsService, times(1)).getTopContents(any());
+
+            action.andExpect(status().isOk())
+                    .andExpect(jsonPath("$[0]['id']").value(mocks.get(0).getId()))
+                    .andExpect(jsonPath("$[0]['name']").value(mocks.get(0).getName()))
+                    .andExpect(jsonPath("$[0]['author']").value(mocks.get(0).getAuthor()))
+                    .andExpect(jsonPath("$[0]['type']").value(mocks.get(0).getType().name()))
+                    .andExpect(jsonPath("$[0]['coin']").value(mocks.get(0).getCoin()))
+                    .andExpect(jsonPath("$[0]['openDate']").value(mocks.get(0).getOpenDate()))
+                    .andExpect(jsonPath("$[0]['sum']").value(mocks.get(0).getSum()));
+        }
+
+        @Test
+        @DisplayName("컨텐츠가 없다면 204 를 반환 한다.")
+        void testGetTopContentsByEmpty() throws Exception {
+
+            when(contentsService.getTopContents(any())).thenReturn(Collections.emptyList());
+
+            ResultActions action = mockMvc.perform(MockMvcRequestBuilders.get("/top-contents")
+                    .param("type", Evaluation.GOOD.name())
+                    .contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
+                    .andDo(print());
+
+            verify(contentsService, times(1)).getTopContents(any());
+
+            action.andExpect(status().is2xxSuccessful());
+        }
+
     }
 
-    @Test
+    @Nested
     @DisplayName("특정 작품을 유료, 무료로 변경 할 수 있는 API")
-    void patchContents() throws Exception {
+    public class PatchContents {
 
-        final UpdatedContentsRequest dto = new UpdatedContentsRequest(Policy.PAGAR, "100");
+        @Test
+        @DisplayName("는 성공적으로 동작을 한다.")
+        void testPatchContents() throws Exception {
 
-        Optional<Contents> mockOptional = Optional.of(ContentsMock.createdMock());
+            final UpdatedContentsRequest dto = new UpdatedContentsRequest(Policy.PAGAR, "100");
 
-        BDDMockito.given(contentsService.updatedTypeAndCoin(any(), any())).willReturn(mockOptional);
+            Optional<Contents> mockOptional = Optional.of(ContentsMock.createdMock());
 
-        ResultActions action =
-                mockMvc
-                        .perform(
-                                MockMvcRequestBuilders.patch("/contents/" + 1)
-                                        .content(JsonUtil.convertObjectToJson(dto))
-                                        .contentType(MediaType.APPLICATION_JSON)
-                                        .characterEncoding("UTF-8"))
-                        .andDo(print());
+            when(contentsService.updatedTypeAndCoin(any() , any())).thenReturn(mockOptional);
 
-        BDDMockito.then(contentsService).should().updatedTypeAndCoin(any(), any());
+            ResultActions action = mockMvc.perform(MockMvcRequestBuilders.patch("/contents/" + 1)
+                    .content(JsonUtil.convertObjectToJson(dto))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding("UTF-8")).andDo(print());
 
-        Contents mock = mockOptional.get();
+            verify(contentsService , times(1))
+                    .updatedTypeAndCoin(any() , any());
 
-        action
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$['id']").value(mock.getId()))
-                .andExpect(jsonPath("$['name']").value(mock.getName()))
-                .andExpect(jsonPath("$['author']").value(mock.getAuthor()))
-                .andExpect(jsonPath("$['type']").value(mock.getType().name()))
-                .andExpect(jsonPath("$['adult']").value(mock.getAdult().name()))
-                .andExpect(jsonPath("$['coin']").value(mock.getCoin()))
-                .andExpect(jsonPath("$['openDate']").value(mock.getOpenDate()));
+            Contents mock = mockOptional.get();
+
+            action.andExpect(status().isOk())
+                    .andExpect(jsonPath("$['id']").value(mock.getId()))
+                    .andExpect(jsonPath("$['name']").value(mock.getName()))
+                    .andExpect(jsonPath("$['author']").value(mock.getAuthor()))
+                    .andExpect(jsonPath("$['type']").value(mock.getType().name()))
+                    .andExpect(jsonPath("$['adult']").value(mock.getAdult().name()))
+                    .andExpect(jsonPath("$['coin']").value(mock.getCoin()))
+                    .andExpect(jsonPath("$['openDate']").value(mock.getOpenDate()));
+
+        }
+
+        @Test
+        @DisplayName("는 데이터가 없다면, 204 를 반환한다.")
+        void testPatchContentsByEmptyCase() throws Exception {
+
+            final UpdatedContentsRequest dto = new UpdatedContentsRequest(Policy.PAGAR, "100");
+
+            when(contentsService.updatedTypeAndCoin(any() , any())).thenReturn(Optional.empty());
+
+            ResultActions action = mockMvc.perform(MockMvcRequestBuilders.patch("/contents/" + 1)
+                    .content(JsonUtil.convertObjectToJson(dto)).contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
+                    .andDo(print());
+
+            verify(contentsService , times(1)).updatedTypeAndCoin(any() , any());
+
+            action.andExpect(status().is2xxSuccessful());
+        }
+
+
     }
 
-    @Test
+
+    @Nested
     @DisplayName("작품 전체 조회 API")
-    void getContents() throws Exception {
+    public class GetContents {
+    @Test
+    @DisplayName("는 정상적으로 동작한다.")
+    void testGetContents() throws Exception {
 
         PageContentsRequest dto = DtoMock.getSelectContentsStoreDTO();
 
         Page<ContentsInfo> mocks = ContentsMock.getPageContentsInfo();
 
-        BDDMockito.given(contentsService.getContents(any())).willReturn(mocks);
+        when(contentsService.getContents(any())).thenReturn(mocks);
 
-        ResultActions action =
-                mockMvc
-                        .perform(
-                                MockMvcRequestBuilders.get("/contents")
-                                        .param("type", dto.getType().name())
-                                        .param("page", dto.getPage().toString())
-                                        .param("size", dto.getSize().toString())
-                                        .contentType(MediaType.APPLICATION_JSON)
-                                        .characterEncoding("UTF-8"))
-                        .andDo(print());
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.get("/contents")
+                .param("type", dto.getType().name())
+                .param("page", dto.getPage().toString())
+                .param("size", dto.getSize().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"))
+                .andDo(print());
 
-        BDDMockito.then(contentsService).should().getContents(any());
+        verify(contentsService , times(1)).getContents(any());
 
-        action
-                .andExpect(status().isOk())
+        action.andExpect(status().isOk())
                 .andExpect(jsonPath("$['content'][0]['id']").value(mocks.getContent().get(0).getId()))
                 .andExpect(jsonPath("$['content'][0]['name']").value(mocks.getContent().get(0).getName()))
-                .andExpect(
-                        jsonPath("$['content'][0]['type']").value(mocks.getContent().get(0).getType().name()))
-                .andExpect(
-                        jsonPath("$['content'][0]['openDate']").value(mocks.getContent().get(0).getOpenDate()))
-                .andExpect(
-                        jsonPath("$['content'][0]['author']").value(mocks.getContent().get(0).getAuthor()))
+                .andExpect(jsonPath("$['content'][0]['type']").value(mocks.getContent().get(0).getType().name()))
+                .andExpect(jsonPath("$['content'][0]['openDate']").value(mocks.getContent().get(0).getOpenDate()))
+                .andExpect(jsonPath("$['content'][0]['author']").value(mocks.getContent().get(0).getAuthor()))
                 .andExpect(jsonPath("$['content'][0]['coin']").value(mocks.getContent().get(0).getCoin()));
     }
-
     @Test
-    @DisplayName("컨텐츠 하나만 조회 API")
-    void getContentsOne() throws Exception {
+    @DisplayName("는 빈데이터를 반환을 하면 204를 반환을 한다.")
+    void testGetContentsByEmptyCase() throws Exception {
 
-        Optional<ContentsInfo> mockOptional = Optional.of(ContentsMock.getContentsInfo());
+        PageContentsRequest dto = DtoMock.getSelectContentsStoreDTO();
 
-        BDDMockito.given(contentsService.getContentsOne(any())).willReturn(mockOptional);
+        Page<ContentsInfo> mocks = ContentsMock.getPageContentsInfo();
 
-        ResultActions action =
-                mockMvc
-                        .perform(
-                                MockMvcRequestBuilders.get("/contents/" + 1)
-                                        .contentType(MediaType.APPLICATION_JSON)
-                                        .characterEncoding("UTF-8"))
-                        .andDo(print());
+        when(contentsService.getContents(any())).thenReturn(Page.empty());
 
-        BDDMockito.then(contentsService).should().getContentsOne(any());
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.get("/contents")
+                .param("type", dto.getType().name())
+                .param("page", dto.getPage().toString())
+                .param("size", dto.getSize().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"))
+                .andDo(print());
 
-        ContentsInfo mock = mockOptional.get();
+        verify(contentsService , times(1)).getContents(any());
 
-        action
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$['id']").value(mock.getId()))
-                .andExpect(jsonPath("$['name']").value(mock.getName()))
-                .andExpect(jsonPath("$['author']").value(mock.getAuthor()))
-                .andExpect(jsonPath("$['type']").value(mock.getType().name()))
-                .andExpect(jsonPath("$['openDate']").value(mock.getOpenDate()));
+        action.andExpect(status().is2xxSuccessful());
     }
+
+
+    }
+
+    @Nested
+    @DisplayName("컨텐츠 하나만 조회 API")
+    public class GetContentsOne {
+
+        @Test
+        @DisplayName("는 정상적으로 동작을 한다.")
+        void testGetContentsOne() throws Exception {
+
+            Optional<ContentsInfo> mockOptional = Optional.of(ContentsMock.getContentsInfo());
+
+            when(contentsService.getContentsOne(any())).thenReturn(mockOptional);
+
+            ResultActions action = mockMvc.perform(MockMvcRequestBuilders.get("/contents/" + 1)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding("UTF-8"))
+                    .andDo(print());
+
+
+            verify(contentsService , times(1)).getContentsOne(any());
+
+            ContentsInfo mock = mockOptional.get();
+
+            action.andExpect(status().isOk())
+                    .andExpect(jsonPath("$['id']").value(mock.getId()))
+                    .andExpect(jsonPath("$['name']").value(mock.getName()))
+                    .andExpect(jsonPath("$['author']").value(mock.getAuthor()))
+                    .andExpect(jsonPath("$['type']").value(mock.getType().name()))
+                    .andExpect(jsonPath("$['openDate']").value(mock.getOpenDate()));
+        }
+
+        @Test
+        @DisplayName("는 빈데이터를 리턴하면 204를 반환을 한다.")
+        void testGetContentsOneByEmptyCase() throws Exception {
+
+            Optional<ContentsInfo> mockOptional = Optional.of(ContentsMock.getContentsInfo());
+
+            when(contentsService.getContentsOne(any())).thenReturn(Optional.empty());
+
+            ResultActions action = mockMvc.perform(MockMvcRequestBuilders.get("/contents/" + 1)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .characterEncoding("UTF-8"))
+                    .andDo(print());
+
+
+            verify(contentsService , times(1)).getContentsOne(any());
+
+            ContentsInfo mock = mockOptional.get();
+
+            action.andExpect(status().is2xxSuccessful());
+        }
+
+    }
+
 }
